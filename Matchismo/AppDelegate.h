36a22e473982f7780ed5e1c84ac9643152c41dc0
Timothy Asquith <timothy.asquith@gmail.com>
2014-12-30 17:58:53 +1000

    Initial Commit

---
+++ Matchismo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Matchismo
+//
+//  Created by Tim Asquith on 30/12/2014.
+//  Copyright (c) 2014 Red Ronin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Matchismo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Matchismo
+//
+//  Created by Tim Asquith on 30/12/2014.
+//  Copyright (c) 2014 Red Ronin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
